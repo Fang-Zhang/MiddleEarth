559f9785dffe500072e12926f736296644cb1feb
Walter Zhang <walter.zhangfang@gmail.com>
2022-11-16 09:40:28 +1300

    Adding @thirdweb-dve component.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
+import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThirdwebProvider
+      desiredChainId={ ChainId.Goerli }
+      chainRpc={{
+        [ChainId.Goerli]:'https://goerli.infura.io/v3/333d22ce307148eeac2420c32a12c4e2'
+      }}
+    >
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  )
+  
 }
 
 export default MyApp
