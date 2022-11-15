366c118702b3412d003767ca30ebdf6d3964099a
Walter Zhang <walter.zhangfang@gmail.com>
2022-11-16 12:55:48 +1300

    try to use .env file.

--- components/Home/Listings.js
+++ components/Home/Listings.js
@@ -1,7 +1,27 @@
+import {useEffect, useState} from 'react';
+import Link from 'next/link';
+import { useMarketplace } from '@thirdweb-dev/react';
 
-
+// const marketPlaceAddress = process.env['MARKETPLACE_ADDRESS'];
 
 const Listings = () => {
+    const [listings, setListings] = useState([]);
+    // const marketplace = useMarketplace(marketPlaceAddress);
+    const marketplace = useMarketplace("0x2284eD2822E00E196Ed0D7D3AA603f9E56e65684");
+    console.log(listings);
+
+    useEffect(() => {
+        getListings();
+    }, []);
+
+    const getListings = async () => {
+        try {
+            const list = await marketplace.getActiveListings();
+            // console.log(list);
+            setListings(list);
+        } catch (err) {console.error(err);}
+    }
+
     return (
         <div>
             Listings
