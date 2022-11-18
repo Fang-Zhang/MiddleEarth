01c59bf9e32b7423099f62b25f5fc00c73283c4f
Walter Zhang <walter.zhangfang@gmail.com>
2022-11-19 10:17:14 +1300

    Creating TopNavbar component.

---
+++ layouts/TopNavbarLayout.js
@@ -0,0 +1,12 @@
+import Header from '../components/Header/Header';
+
+const TopNavbarLayout = (children) => {
+    return (
+        <>
+            <Header/>
+            <div>TopNavBar</div>
+        </>
+    )
+}
+
+export default TopNavbarLayout;
