5477d2e485bd9b7f60dc3500708f1867d3d00128
Walter Zhang <walter.zhangfang@gmail.com>
2022-11-22 21:07:37 +1300

    Creating Dropdown component.

---
+++ components/NFTDetails/Dropdown.js
@@ -0,0 +1,12 @@
+import { Disclosure } from '@headlessui/react';
+import {BiChevronUp, BiChevronDown } from 'react-icons/bi';
+
+const style = {};
+
+const DropDown = () => {
+    return (
+        <div>Dropdown Component</div>
+    )
+}
+
+export default DropDown;
--- components/NFTDetails/NFTDetail.js
+++ components/NFTDetails/NFTDetail.js
@@ -1,7 +1,11 @@
 import { GrTextAlignLeft } from 'react-icons/gr';
 import { BsFillBookmarkFill } from 'react-icons/bs';
+import Dropdown from './Dropdown';
 
-const style = {};
+const style = {
+    wrapper: `flex flex-col divide-y rounded-lg overflow-hidden border dark:divide-black dark:border-black`,
+    icon: `h-5 w-5 text-gray-600`,
+};
 
 const NFTDetails = () => {
     const dropdownData = [
@@ -16,7 +20,11 @@ const NFTDetails = () => {
     ]
 
     return (
-        <div>NFT Details</div>
+        <div className={style.wrapper}>
+            {dropdownData.map((item, index) => (
+                <Dropdown />
+            ))}
+        </div>
     )
 }
 
